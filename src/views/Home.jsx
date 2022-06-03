9399a97ca530577e807b5eb938af5217de837019
MeowGuaurr <52300594+MeowGuaurr@users.noreply.github.com>
2022-06-03 15:13:55 -0600

    Update home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,50 +1,22 @@
 import React from 'react'
 
 import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react'
-import { API_URL } from '../data/data'
-import Table from 'react-bootstrap/Table'
 
 const Home = () => {
-    const [city, setCity] = useState([{ idCity: 0, name: '' }]);
-    const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-        fetch(`${API_URL}/city/getAll`)
-            .then((res) => {
-                if (res.ok) {
-                    return res.json();
-                }
-                throw res;
-            })
-            .finally(() => setIsLoading(false));
-    }, []);
-
-    if (isLoading) {
-        return <p>Cargando...</p>
-    }
-
-
-   return (
-       <>
-       <Table responsive>
-           <thead>
-               <tr>
-                   <td>id</td>
-                   <td>name</td>
-               </tr>
-           </thead>
-           <tbody>
-                   {city.map((city, i) =>
-                       <tr>
-                           <td>{city.idCity}</td>
-                           <td>{city.idNamey}</td>
-                       </tr>
-                   )} 
-               
-           </tbody>
-       </Table>
-       </>)
+    return (
+        <div>
+            <h1>Bienvenido al registro de enfermedades</h1>
+            <p>Created by:</p>
+            <ul>
+                <li>Reyna Galan - MeowGuaurr</li>
+                <li>Renatto Baca - RenattoB</li>
+                <li>Rafael Marrugo - GalassoX</li>
+                <li>Mario Rodriguez - MarioRodriguez01</li>
+                <li>Josué Cortez - j0su22 </li>
+            </ul>
+        </div>
+    )
 }
 
-export default Home
+export default Home;
 
